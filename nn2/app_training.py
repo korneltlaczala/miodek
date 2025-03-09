452ea3add87c5714c62ac822c06aa378b9ef83d7
korneltlaczala <kornel.tlaczala@gmail.com>
2025-03-09 22:06:50 +0100

    app launches

--- nn2/app_training.py
+++ nn2/app_training.py
@@ -1,4 +1,5 @@
 import os
+from tkinter import messagebox
 import customtkinter as ctk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from models import *
@@ -14,23 +15,28 @@ class App:
         self.MODEL_DIR = os.path.join(self.BASE_DIR, "models")
 
         self.env = TrainingEnvironment(self, trainer_name, epochs=5e4, learning_rate=0.1, report_interval=1000)
-        self.trainer_frame = TrainerFrame(self.root, self, self.env)
+        # self.env = TrainingEnvironment.load()
+        self.trainer_frame = TrainerFrame(self.root, self)
         self.trainer_frame.pack(side='top', expand=False, fill='both', padx=5, pady=5)
         self.result_frame = ResultFrame(self.root, self)
         self.result_frame.pack(side='bottom', expand=True, fill='both', padx=5, pady=5)
 
         self.root.mainloop()
 
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def on_closing(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.env.save()
+            self.root.destroy()
 
 class TrainerFrame(ctk.CTkFrame):
 
-    def __init__(self, parent, app, env, **kwargs):
+    def __init__(self, parent, app, **kwargs):
         super().__init__(parent, **kwargs)
         self.parent = parent
         self.app = app
-        self.env = env
         self.fill()
-
 
     def fill(self):
         self.save_frame = ctk.CTkFrame(self)
@@ -57,26 +63,24 @@ class TrainerFrame(ctk.CTkFrame):
         self.update_save_frame()
 
     def fill_progress_frame(self):
-        current_epoch = str(self.trainer.current_epoch)
-        mse = str(round(self.trainer.tester.mse, 2))
-        best_mse = str(self.trainer.best_mse)
-        self.current_epoch_label = ctk.CTkLabel(self.progress_frame, text="Current Epoch: " + current_epoch, anchor='w')
+        self.current_epoch_label = ctk.CTkLabel(self.progress_frame, anchor='w')
         self.current_epoch_label.grid(row=0, column=2, padx=5, pady=5, sticky='nsew')
-        self.current_mse_label = ctk.CTkLabel(self.progress_frame, text="Current MSE: " + mse, anchor='w')
+        self.current_mse_label = ctk.CTkLabel(self.progress_frame, anchor='w')
         self.current_mse_label.grid(row=1, column=2, padx=5, pady=5, sticky='nsew')
-        self.best_mse_label = ctk.CTkLabel(self.progress_frame, text="Best MSE: " + best_mse, anchor='w')
+        self.best_mse_label = ctk.CTkLabel(self.progress_frame, anchor='w')
         self.best_mse_label.grid(row=2, column=2, padx=5, pady=5, sticky='nsew')
+        self.update_progress_frame()
 
     def fill_settings_frame(self):
         self.learning_rate_label = ctk.CTkLabel(self.settings_frame, anchor='w')
-        self.learning_rate_label.configure(text=f"Learning Rate: {self.trainer.learning_rate}")
         self.learning_rate_label.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
         self.generate_learning_rate_buttons()
 
         self.new_epochs_label = ctk.CTkLabel(self.settings_frame, anchor='w')
-        self.new_epochs_label.configure(text=f"New Epochs: {self.trainer.new_epochs}")
         self.new_epochs_label.grid(row=2, column=0, padx=5, pady=5, sticky='nsew')
         self.generate_epoch_settings()
+
+        self.update_settings_frame()
 
     def fill_button_frame(self):
         self.start_button = ctk.CTkButton(self.button_frame, text="Start", command=self.env.start_training)
@@ -84,14 +88,19 @@ class TrainerFrame(ctk.CTkFrame):
  
     def update_save_frame(self):
         self.trainer_name_entry.delete(0, 'end')
-        self.trainer_name_entry.insert(0, self.trainer.name)
+        self.trainer_name_entry.insert(0, self.env.trainer_name)
 
     def update_progress_frame(self):
-        current_epoch = str(self.trainer.current_epoch)
-        mse = str(round(self.trainer.tester.mse, 2))
-        # self.current_epoch_label.configure(text="Current Epoch: " + current_epoch)
-        # self.current_mse_label.configure(text="Current MSE: " + mse)
-        print("updating")
+        current_epoch = str(self.env.trainer.current_epoch)
+        mse = str(round(self.env.trainer.tester.mse, 2))
+        best_mse = str(self.env.trainer.best_mse)
+        self.current_epoch_label.configure(text="Current Epoch: " + current_epoch)
+        self.current_mse_label.configure(text="Current MSE: " + mse)
+        self.best_mse_label.configure(text="Best MSE: " + best_mse)
+
+    def update_settings_frame(self):
+        self.learning_rate_label.configure(text=f"Learning Rate: {self.env.learning_rate}")
+        self.new_epochs_label.configure(text=f"New Epochs: {self.env.epochs}")
 
     def generate_learning_rate_buttons(self):
         learning_rates = [1/10**i for i in range(1, 7)]
@@ -104,7 +113,7 @@ class TrainerFrame(ctk.CTkFrame):
 
     def generate_epoch_settings(self):
         values = [str(i) for i in range(1, 11)]
-        self.epoch_multiplier_combobox = ctk.CTkComboBox(self.settings_frame, values=values, command=self.env.set_epoch_multiplier())
+        self.epoch_multiplier_combobox = ctk.CTkComboBox(self.settings_frame, values=values, command=self.env.set_epoch_multiplier)
         self.epoch_multiplier_combobox.grid(row=3, column=0, padx=5, pady=5, sticky='nsew')
 
         epoch_choices = [10**i for i in range(3, 7)]
@@ -115,8 +124,9 @@ class TrainerFrame(ctk.CTkFrame):
             button.grid(row=3, column=i+1, padx=5, pady=5, sticky='nsew')
             self.epoch_choices_buttons.append(button)
 
-        self.epoch_multiplier = int(self.epoch_multiplier_combobox.get())
-        self.epoch_oom = epoch_choices[0]
+    @property
+    def env (self):
+        return self.app.env
 
 class ResultFrame(ctk.CTkFrame):
 
@@ -149,8 +159,11 @@ class ResultFrame(ctk.CTkFrame):
         self.new_fit_canvas.get_tk_widget().pack(side='right', fill='both', expand=True)
     
     def get_fit_fig(self):
-        return self.app.trainer.tester.get_fig(linear=True)
+        return self.env.trainer.tester.get_fig(linear=True)
 
+    @property
+    def env(self):
+        return self.app.env
 
 class TrainingEnvironment():
 
@@ -170,22 +183,25 @@ class TrainingEnvironment():
         self.epochs = epochs
         self.epoch_multiplier = 1
         self.epoch_oom = epochs
+        self.app
 
     def set_learning_rate(self, learning_rate):
         self.learning_rate = learning_rate
-        self.app.trainer_frame.learning_rate_label.configure(text=f"Learning Rate: {self.learning_rate}")
+        self.app.update_settings_frame()
+        # self.app.trainer_frame.learning_rate_label.configure(text=f"Learning Rate: {self.learning_rate}")
 
     def set_epoch_multiplier(self):
         self.epoch_multiplier = int(self.app.trainer_frame.epoch_multiplier_combobox.get())
-        self.set_new_epochs()
+        self.set_epochs()
 
     def set_epoch_oom(self, epoch_oom):
         self.epoch_oom = epoch_oom
-        self.set_new_epochs()
+        self.set_epochs()
 
-    def set_new_epochs(self):
-        self.new_epochs = self.epoch_multiplier * self.epoch_oom
-        self.app.trainer_frame.new_epochs_label.configure(text=f"New Epochs: {self.new_epochs}")
+    def set_epochs(self):
+        self.epochs = self.epoch_multiplier * self.epoch_oom
+        self.app.update_settings_frame()
+        # self.app.trainer_frame.new_epochs_label.configure(text=f"New Epochs: {self.epochs}")
 
     def start_training(self):
         self.app.trainer.train(epochs=self.new_epochs,
@@ -216,3 +232,4 @@ class TrainingEnvironment():
         
 if __name__ == "__main__":
     App(trainer_name="trainer_app")
+#   # App()
