75128c62124cb452371a52c5c1a128b8aa51e20d
korneltlaczala <kornel.tlaczala@gmail.com>
2025-05-21 03:45:20 +0200

    playing with params

--- ae1/main.py
+++ ae1/main.py
@@ -22,21 +22,21 @@ class Population():
                vector.append(random.uniform(self.interval[0], self.interval[1]))
             self.population.append(vector)
 
-    def evolve(self, generations=10, verbose=False, report_count=1):
-        report_interval = int(generations / report_count)
+    def evolve(self, generations=10, verbose=False, report_interval=10):
         for i in range(generations):
             self.pass_generation(verbose)
             if i % report_interval == 0:
-                print(f"generation: {i}:\t", end="")
-                self.evaluate()
+                print(f"generation: {i}\t\tbest value: {self.evaluate()}")
 
-        self.evaluate()
+        self.summary()
 
     def pass_generation(self, verbose):
         self.age += 1
+        old_population = self.population
         children = self.generate_children(self.population)                                  # krzyżowanie
         mutated_children = self.mutate(children)                                            # mutacja
         new_population = self.select(parents=self.population, children=mutated_children)    # selekcja
+
         self.population = new_population
 
         if verbose:
@@ -46,11 +46,11 @@ class Population():
             # print(f"children count: {len(children)}")
             # print(f"new population size: {len(self.population)}")
             small_size = 10
-            alpha = 0.1
-            samples = [self.population, children, mutated_children, new_population]
-            colors = ["blue", "red", "purple", "green"]
-            sizes = [30, small_size, small_size, small_size]
-            alphas = [1, alpha, alpha, 0.7]
+            alpha = 0.0
+            samples = [old_population, children, mutated_children, new_population]
+            colors = ["blue", "green", "purple", "red"]
+            sizes = [50, small_size, small_size, 20]
+            alphas = [1, alpha, alpha, 1]
             self.plot(samples, colors=colors, sizes=sizes, alphas=alphas)
 
     def generate_children(self, population, parent_density=0.7):
@@ -61,7 +61,7 @@ class Population():
         return children
 
     def mutate(self, children):
-        mutator = Mutator(children, 0.1)
+        mutator = Mutator(children, mutation_scale=0.1, permutation_chance=0.2)
         children = mutator.gaussian(children)
         return children
 
@@ -93,15 +93,26 @@ class Population():
         for vector in population:
             print(vector)
 
+    def find_min(self):
+        values = self.fitness_function.apply(self.population)
+        return self.population[np.argmin(values)]
+
+    def summary(self):
+        values = self.fitness_function.apply(self.population)
+        v_min = self.find_min()
+        print(f"best value: {min(values)}")
+        print(f"best vector: {v_min}")
+
     def evaluate(self):
         values = self.fitness_function.apply(self.population)
-        print(f"best value: {min(values)}")
+        return min(values)
+
     
 
 if __name__ == "__main__":
-    # population = Population(200, BasicFunction(), [-10, 10])
-    population = Population(200, RastriginFunction(dim=5), [-10, 10])
-    n = 100
-    # population.evolve(generations=n, report_count=n, verbose=True)
-    population.evolve(generations=n, report_count=n)
+    population = Population(30, BasicFunction(), [-10, 10])
+    # population = Population(50, RastriginFunction(dim=5), [-10, 10])
+    n = 600
+    # population.evolve(generations=n, report_interval=1, verbose=True)
+    population.evolve(generations=n, report_interval=10)
     population.plot_population()
